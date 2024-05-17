09cdedbc204ed49bee9e59b974a4522e3c827dfb
Christopher Sharkey <ev2900@gmail.com>
2024-05-17 09:01:02 -0600

    Update sample_job.py

--- sample_job.py
+++ sample_job.py
@@ -4,13 +4,18 @@ from pyspark.sql import SparkSession
 from pyspark.sql.functions import col, to_timestamp, monotonically_increasing_id, to_date, when
 from pyspark.sql.types import *
 from datetime import datetime
+import sys
+from awsglue.utils import getResolvedOptions
+
+# Get S3 bucket name from job parameters
+args = getResolvedOptions(sys.argv, ['s3-bucket-name'])
 
 # Initialize a SparkSession
 spark = SparkSession.builder \
     .appName("IcebergIntegration") \
     .config("spark.sql.extensions", "org.apache.iceberg.spark.extensions.IcebergSparkSessionExtensions") \
     .config("spark.sql.catalog.glue_catalog", "org.apache.iceberg.spark.SparkCatalog") \
-    .config("spark.sql.catalog.glue_catalog.warehouse", "s3://<s3_bucket>/iceberg/") \
+    .config("spark.sql.catalog.glue_catalog.warehouse", "s3://" + args['s3-bucket-name'] + "/iceberg/") \
     .config("spark.sql.catalog.glue_catalog.catalog-impl", "org.apache.iceberg.aws.glue.GlueCatalog") \
     .config("spark.sql.catalog.glue_catalog.io-impl", "org.apache.iceberg.aws.s3.S3FileIO") \
     .getOrCreate()
